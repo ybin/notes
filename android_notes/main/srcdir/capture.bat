c2d91cb443e7d0d37d24d702fc47b878fc7f7b28
ybin sun <ybin.sun@gmail.com>
2013-10-11 11:35:42 +0800

    add android notes: capture screen on PC.

---
+++ android_notes/main/srcdir/capture.bat
@@ -0,0 +1,14 @@
+﻿@echo off
+::
+:: capture android screen and copy png file to *current* directory.
+::
+set blablafname=
+for /f "usebackq" %%a in (`echo %date%`) do set "blablafname=%%a"
+for /f "usebackq delims=: tokens=1-3" %%a in (`echo %time%`) do (
+	set "blablafname=%blablafname%-%%a-%%b-%%c"
+)
+
+adb shell screencap /sdcard/screenblablablablabla.png
+adb pull /sdcard/screenblablablablabla.png %blablafname%.png > nul 2>&1 
+echo Save screen capture to %blablafname%.png
+adb shell rm /sdcard/screenblablablablabla.png
