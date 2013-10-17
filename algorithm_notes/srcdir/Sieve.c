2018ccc5e20dc03cbc6c726ec67304d5ee23ebf7
ybin sun <ybin.sun@gmail.com>
2013-10-17 17:18:23 +0800

    add notes, linux note: compress & uncompress command; algorithm notes: Sieve algorithm for searching all primes less than N.

---
+++ algorithm_notes/srcdir/Sieve.c
@@ -0,0 +1,21 @@
+﻿
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void sieve(int n) {
+	char sieve[n + 1];
+	for(int i = 2; i <= n; i++)
+		sieve[i] = 1;
+	for(int i = 2; i <= sqrt(n) + 1; i++) {
+		if(sieve[i])
+			for(int j = i; j * i <= n; j++) {
+				sieve[j * i] = 0;
+		}
+	}
+	for (int i = 2; i <= n; ++i) {
+		if(sieve[i]) {
+			printf(" %d\n", i);
+		}
+	}
+}
